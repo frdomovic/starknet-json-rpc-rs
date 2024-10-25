0782b8b8f3bb8a1696267d7deecce440eb151501
alenmestrov <alenmestrov@gmail.com>
2024-10-25 10:21:20 +0200

    serialized request

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 use bs58;
 use hex;
-use starknet_core::types::{
-    BlockId, BlockTag, BroadcastedInvokeTransaction, BroadcastedInvokeTransactionV1, ContractClass,
-    Felt, FunctionCall, InvokeTransactionV1,
+use starknet::accounts::{Account, ExecutionEncoding, SingleOwnerAccount};
+use starknet::core::chain_id;
+use starknet::core::crypto::ExtendedSignature;
+use starknet::core::types::{
+    BlockId, BlockTag, BroadcastedInvokeTransaction, BroadcastedInvokeTransactionV1, Call, CallType, ComputationResources, ContractClass, EntryPointType, Felt, FunctionCall, FunctionInvocation, InvokeTransactionV1
 };
-use starknet_core::utils::get_selector_from_name;
+use starknet::core::utils::get_selector_from_name;
+use starknet::providers::sequencer::models::InvokeFunctionTransaction;
+use starknet::signers::{LocalWallet, SigningKey};
 use starknet_crypto::{poseidon_hash_many, Signature};
-use starknet_providers::jsonrpc::HttpTransport;
-use starknet_providers::{JsonRpcClient, Provider, Url};
+use starknet::providers::jsonrpc::HttpTransport;
+use starknet::providers::{JsonRpcClient, Provider, Url};
+use core::str;
 use std::collections::BTreeMap;
+use std::str::Bytes;
 use std::{borrow::Cow, str::FromStr};
 use thiserror::Error;
-use types::{Application, ContextId, ContextIdentity};
+use types::{ContextId, ContextIdentity};
+// use starknet_core::codec::{ArrayTrait, Encode};
+use starknet::core::codec::Encode;
 
+use serde::{Serialize, Deserialize};
 mod types;
 
 #[derive(Debug)]
@@ -74,14 +83,15 @@ fn compute_transaction_hash(
     poseidon_hash_many(&elements)
 }
 
-async fn sign_transaction(hash: &Felt, secret_key: &Felt) -> Result<Signature, StarknetError> {
-    let signature = starknet_core::crypto::ecdsa_sign(secret_key, hash);
-    match signature {
-        Ok(result) => Ok(result.into()),
-        Err(_) => Err(StarknetError::InvalidResponse {
-            operation: ErrorOperation::Query,
-        }),
-    }
+async fn sign_transaction(hash: &Felt, secret_key: &Felt) -> Result<ExtendedSignature, StarknetError> {
+    let signature = starknet::core::crypto::ecdsa_sign(secret_key, hash).unwrap();
+    Ok(signature)
+    // match signature {
+    //     Ok(result) => Ok(result.into()),
+    //     Err(_) => Err(StarknetError::InvalidResponse {
+    //         operation: ErrorOperation::Query,
+    //     }),
+    // }
 }
 
 #[derive(Debug, Error)]
@@ -108,20 +118,20 @@ pub enum StarknetError {
     },
 }
 
-fn application_to_felt_vec(application: &Application) -> Vec<Felt> {
-    let mut felt_vec = Vec::new();
+// fn application_to_felt_vec(application: &Application) -> Vec<Felt> {
+//     let mut felt_vec = Vec::new();
     
-    // Add single fields
-    felt_vec.push(application.id);
-    felt_vec.push(application.blob);
-    felt_vec.push(application.size);
+//     // Add single fields
+//     felt_vec.push(application.id);
+//     felt_vec.push(application.blob);
+//     felt_vec.push(application.size);
     
-    // Add collection fields (source and metadata)
-    felt_vec.extend(application.source.iter().cloned());
-    felt_vec.extend(application.metadata.iter().cloned());
+//     // Add collection fields (source and metadata)
+//     felt_vec.extend(application.source.iter().cloned());
+//     felt_vec.extend(application.metadata.iter().cloned());
     
-    felt_vec
-}
+//     felt_vec
+// }
 
 
 #[derive(Copy, Clone, Debug, Error)]
@@ -228,6 +238,9 @@ impl Network {
         let entry_point_selector = get_selector_from_name(method)
             .unwrap_or_else(|_| panic!("Failed to convert method name to entry point selector"));
 
+
+
+
         let calldata: Vec<Felt> = if args.is_empty() {
             vec![]
         } else {
@@ -258,38 +271,38 @@ impl Network {
             Felt::from_str("0x0").unwrap(),
         ];
 
-        let application = Application {
-            id: Felt::from_raw(6382179),
-            blob: 7092165981550440290i64.into(),
-            size: 0.into(),
-            source: [7161124082558530159i64.into()].to_vec(),
-            metadata: [1835365473.into()].to_vec(),
-        };
+        // let application = Application {
+        //     id: Felt::from_raw(6382179),
+        //     blob: 7092165981550440290i64.into(),
+        //     size: 0.into(),
+        //     source: [7161124082558530159i64.into()].to_vec(),
+        //     metadata: [1835365473.into()].to_vec(),
+        // };
 
-        let calldata = application_to_felt_vec(&application);
+        // let calldata = application_to_felt_vec(&application);
 
-        let invoke_transaction_v1 = BroadcastedInvokeTransactionV1 {
-            sender_address,
-            calldata,
+        // let invoke_transaction_v1 = BroadcastedInvokeTransactionV1 {
+        //     sender_address,
+        //     calldata,
 
-            max_fee: Felt::from(304139049569u64),
-            signature: signature_vec,
-            nonce,
-            is_query: false,
-        };
-        let broadcasted_transaction = BroadcastedInvokeTransaction::V1(invoke_transaction_v1);
-        let response = self
-            .client
-            .add_invoke_transaction(&broadcasted_transaction)
-            .await;
-        match response {
-            Ok(result) => {
-                println!("Transaction successful: {:?}", result);
-            }
-            Err(err) => {
-                eprintln!("Error adding invoke transaction: {:?}", err);
-            }
-        }
+        //     max_fee: Felt::from(304139049569u64),
+        //     signature: signature_vec,
+        //     nonce,
+        //     is_query: false,
+        // };
+        // let broadcasted_transaction = BroadcastedInvokeTransaction::V1(invoke_transaction_v1);
+        // let response = self
+        //     .client
+        //     .add_invoke_transaction(&broadcasted_transaction)
+        //     .await;
+        // match response {
+        //     Ok(result) => {
+        //         println!("Transaction successful: {:?}", result);
+        //     }
+        //     Err(err) => {
+        //         eprintln!("Error adding invoke transaction: {:?}", err);
+        //     }
+        // }
     }
 
     async fn get_nonce(&self, contract_id: &str) -> Result<Felt, StarknetError> {
@@ -406,25 +419,214 @@ async fn test2() {
     // let author_id: ContextIdentity =
     //     Felt::from_str("0x660ad6d4b87091520b5505433340abdd181a00856443010fa799f945d2dd5da")
     //         .unwrap();
+
+    let alice_key = starknet::signers::SigningKey::from_random();
+    let alice_key_felt = starknet::signers::SigningKey::secret_scalar(&alice_key);
+    let alice_public_key = alice_key.verifying_key();
+    let alice_public_key_felt = alice_public_key.scalar();
+    println!("Alice Signing key: {:?}", alice_key);
+    println!("Alice Public key: {:?}", alice_public_key);
+
+    let context_key = starknet::signers::SigningKey::from_random();
+    let context_public_key = context_key.verifying_key();
+    let context_public_key_felt = context_public_key.scalar();
+    println!("Context Signing key: {:?}", context_key);
+    println!("Context Public key: {:?}", context_public_key);
+
     let application = Application {
         id: 6382179.into(),
         blob: 7092165981550440290i64.into(),
-        size: 0.into(),
-        source: [7161124082558530159i64.into()].to_vec(),
-        metadata: [1835365473.into()].to_vec(),
+        size: 0,
+        source: "https://github.com/calimero-network/core".as_bytes().to_vec(),
+        metadata: "some metadata".as_bytes().to_vec(),
     };
 
-    println!("Application: {:?}", application);
+    let request = Request {
+        signer_id: alice_public_key_felt,
+        nonce: 0,
+        kind: RequestKind::Context(
+          ContextRequest {
+            context_id: context_public_key_felt,
+            kind: ContextRequestKind::Add(
+              context_public_key_felt,
+              application.clone()
+            ),
+        }),
+    };
+
+    // let mut serialized = Vec::new();
+    // request.serialize(&mut serialized);
+    let mut serialized = vec![];
+    let _ = request.encode(&mut serialized);
+    println!("Serialized: {:?}", serialized);
+
+    let hash = poseidon_hash_many(&serialized);
+    println!("Hash: {:?}", hash);
+
+    let signature = alice_key.sign(&hash).unwrap();
+    println!("Signature: {:?}", signature);
+
+    let signed_request = Signed {
+        payload: serialized,
+        signature: vec![signature.r, signature.s],
+    };
+
+    println!("Signed Request: {:?}", signed_request);
+
+    let mut signed_request_serialized = vec![];
+    let _ = signed_request.encode(&mut signed_request_serialized);
+
+    //Call the contract
+    let provider = JsonRpcClient::new(HttpTransport::new(
+        Url::parse("https://starknet-sepolia.public.blastapi.io/rpc/v0_7").unwrap(),
+    ));
+
+    let signer = LocalWallet::from(SigningKey::from_secret_scalar(
+        Felt::from_hex("0x3466a2196c72a94edd80c49baaad89c3cd71815038c31e1d3c94337ad97406d").unwrap(),
+    ));
+    let address = Felt::from_hex("0x008f4a3c215d4f5b2c6c2cf58ad4cd5f8ea55be51c816b20a81f1940ab7724b4").unwrap();
+    let tst_token_address =
+        Felt::from_hex("07394cbe418daa16e42b87ba67372d4ab4a5df0b05c6e554d158458ce245bc10").unwrap();
+
+    let mut account = SingleOwnerAccount::new(
+        provider,
+        signer,
+        address,
+        chain_id::SEPOLIA,
+        ExecutionEncoding::New,
+    );
+
+    // `SingleOwnerAccount` defaults to checking nonce and estimating fees against the latest
+    // block. Optionally change the target block to pending with the following line:
+    account.set_block_id(BlockId::Tag(BlockTag::Pending));
+
+    let result = account
+        .execute_v3(vec![Call {
+            to: Felt::from_str(contract_id).unwrap(),
+            selector: Felt::from_str("0x33f8af2c6d5b2376345a3c43ad230b0741fb5694c7064741c1927142bbd442a").unwrap(),
+            calldata: signed_request_serialized,
+        }])
+        .send()
+        .await
+        .unwrap();
+
+    println!("{:?}", result);
 
 
-    network.mutate(contract_id, method, args).await;
+    // network.mutate(contract_id, method, signed_request).await;
 }
 
-pub struct SignedReq {
+#[derive(Debug, Encode)]
+pub struct Signed {
     payload: Vec<Felt>,
-    signature: (Felt, Felt),
-    public_key: Felt,
+    signature: Vec<Felt>,
 }
+
+#[derive(Encode)]
+pub struct Request {
+    kind: RequestKind,
+    signer_id: ContextIdentity,
+    nonce: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Encode)]
+pub enum RequestKind {
+    Context(ContextRequest),
+}
+
+#[derive(Debug, Serialize, Deserialize, Encode)]
+pub struct ContextRequest {
+    pub context_id: ContextId,
+    pub kind: ContextRequestKind,
+}
+
+#[derive(Debug, Serialize, Deserialize, Encode)]
+pub enum ContextRequestKind {
+    Add(ContextIdentity, Application),
+    // UpdateApplication(Application),
+    // AddMembers(Vec<ContextIdentity>),
+    // RemoveMembers(Vec<ContextIdentity>),
+    // Grant(Vec<(ContextIdentity, Capability)>),
+    // Revoke(Vec<(ContextIdentity, Capability)>),
+}
+
+pub struct Context {
+  pub application: Application,
+  pub member_count: u32,
+}
+
+// Context Application
+#[derive(Debug, Serialize, Deserialize, Clone, Encode)]
+pub struct Application {
+  pub id: Felt,  // Represents [u8; 32]
+  pub blob: Felt,  // Represents [u8; 32]
+  pub size: u64,
+  pub source: Vec<u8>,  // Represents ApplicationSource
+  pub metadata: Vec<u8>  // Represents ApplicationMetadata
+}
+
+// impl<T: Serialize> Signed<T> {
+//   pub fn new<R, F>(payload: &T, sign: F) -> Result<Self, ConfigError<R::Error>>
+//   where
+//       R: IntoResult<Signature>,
+//       F: FnOnce(&[u8]) -> R,
+//   {
+//       let payload = serde_json::to_vec(&payload)?.into_boxed_slice();
+
+//       let signature = sign(&payload)
+//           .into_result()
+//           .map_err(ConfigError::DerivationError)?;
+
+//       Ok(Self {
+//           payload: Repr::new(payload),
+//           signature: Repr::new(signature),
+//       })
+//   }
+// }
+
+// pub trait IntoResult<T> {
+//   type Error;
+
+//   fn into_result(self) -> Result<T, Self::Error>;
+// }
+
+// impl<T> IntoResult<T> for T {
+//   type Error = Infallible;
+
+//   fn into_result(self) -> Result<T, Self::Error> {
+//       Ok(self)
+//   }
+// }
+
+// impl<T, E> IntoResult<T> for Result<T, E> {
+//   type Error = E;
+
+//   fn into_result(self) -> Result<T, Self::Error> {
+//       self
+//   }
+// }
+
+// impl<'a, T: Deserialize<'a>> Signed<T> {
+//   pub fn parse<R, F>(&'a self, f: F) -> Result<T, ConfigError<R::Error>>
+//   where
+//       R: IntoResult<SignerId>,
+//       F: FnOnce(&T) -> R,
+//   {
+//       let parsed = serde_json::from_slice(&self.payload)?;
+
+//       let bytes = f(&parsed)
+//           .into_result()
+//           .map_err(ConfigError::DerivationError)?;
+
+//       let key = bytes
+//           .rt::<VerifyingKey>()
+//           .map_err(ConfigError::VerificationKeyParseError)?;
+
+//       key.verify(&self.payload, &self.signature)
+//           .map_or(Err(ConfigError::InvalidSignature), |()| Ok(parsed))
+//   }
+// }
+
 
 // #[serde_as]
 // #[derive(Serialize, Deserialize, Debug)]
@@ -482,3 +684,4 @@ pub struct SignedReq {
 //         format!("Decimal: {}, Hex: 0x{:x}", value.to_string(), value)
 //     }
 // }
+
--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 
-use starknet_core::types::Felt;
+use serde::{Serialize, Deserialize};
+use starknet::core::types::Felt;
 
 pub type ContextId = Felt;
 
@@ -8,20 +9,6 @@ pub type ContextId = Felt;
 pub type ContextIdentity = Felt;
 
 // Context
-pub struct Context {
-    pub application: Application,
-    pub member_count: u32,
-}
-
-// Context Application
-#[derive(Debug)]
-pub struct Application {
-    pub id: Felt,  // Represents [u8; 32]
-    pub blob: Felt,  // Represents [u8; 32]
-    pub size: Felt,
-    pub source: Vec<Felt>,  // Represents ApplicationSource
-    pub metadata: Vec<Felt>,  // Represents ApplicationMetadata
-}
 
 // Context Config
 pub struct Config {
@@ -50,29 +37,4 @@ impl Into<Felt> for Capability {
             Capability::ManageMembers => Felt::from_str("0x0").unwrap(),
         }
     }
-}
-
-pub struct Signed {
-    pub payload: Vec<Felt>,
-    pub signature: (Felt, Felt),  // (r, s) of the signature
-    pub public_key: Felt,
-}
-
-pub struct Request {
-    pub kind: RequestKind,
-    pub signer_id: ContextIdentity,
-    pub timestamp_ms: u64,
-}
-
-pub enum RequestKind {
-    Context(ContextRequest),
-}
-
-pub struct ContextRequest {
-    pub context_id: ContextId,
-    pub kind: ContextRequestKind,
-}
-
-pub enum ContextRequestKind {
-    Add(ContextIdentity, Application),
 }
